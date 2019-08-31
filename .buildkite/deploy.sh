023dcaaabab82ba4d4eb57754bb4c7e45f65df91
Andrew Widdowson <drewtechau@gmail.com>
2019-08-31 14:13:23 +1000

    fix bucket name for deploy script

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 # Create packaged template and upload to S3
-sam package --template-file template.yml --s3-bucket sam-buildkite-deployment-test --output-template-file packaged.yml
+sam package --template-file template.yml --s3-bucket drewtechau-aws-deploy --output-template-file packaged.yml
 
 # Apply CloudFormation template
-sam deploy --template-file ./packaged.yml --stack-name sam-buildkite-deployment-test --capabilities CAPABILITY_IAM
+sam deploy --template-file ./packaged.yml --stack-name drewtech-buildkite-deployment-test --capabilities CAPABILITY_IAM
 
