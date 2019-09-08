3f1018d656dfaadc3ee3934ad0470fa4a0cbbd98
Andrew Widdowson <drewtechau@gmail.com>
2019-09-08 12:08:30 +1000

    update deploy shell script

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 
 # Create packaged template and upload to S3
-sam package --template-file template.yml --s3-bucket drewtechau-aws-deploy --output-template-file packaged.yml
+# sam package --template-file template.yml --s3-bucket drewtechau-aws-deploy --output-template-file packaged.yml
 
 # Apply CloudFormation template
-sam deploy --template-file ./packaged.yml --stack-name drewtech-buildkite-deployment-test --capabilities CAPABILITY_IAM
+/sam deploy --template-file ./packaged.yml --stack-name drewtech-buildkite-deployment-test --capabilities CAPABILITY_IAM
+
+cd terraform
+terraform init
+terraform apply -auto-approve
 
